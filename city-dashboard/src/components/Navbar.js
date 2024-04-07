ead57f3c5e83c3db523daafdee74d79e0cf85dcd
Emma <126622943+escally615@users.noreply.github.com>
2024-04-07 14:58:18 +0000

    moved navbar

--- city-dashboard/src/components/Navbar.js
+++ city-dashboard/src/components/Navbar.js
@@ -4,12 +4,6 @@ function Navbar() {
   return (
     <nav className="navbar">
      <h1>Plan ***Chicago***</h1>
-      <ul className="nav-items">
-        <li><button onClick={() => window.location.href="/pages/CrimeMap.html"}>Crime Map</button></li>
-        <li><button onClick={() => window.location.href="/pages/TransitOutages.html"}>Transit Outages</button></li>
-        <li><button onClick={() => window.location.href="/pages/ReportIssue.html"}>Report An Issue</button></li>
-        {/* Add more navigation items here */}
-      </ul>
     </nav>
   );
 }
